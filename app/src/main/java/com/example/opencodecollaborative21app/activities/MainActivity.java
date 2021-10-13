0124da49f4136b2ddcb437aba8e57236f5d8af56
raghavgoel25 <75788719+raghavgoel25@users.noreply.github.com>
2021-10-13 19:17:43 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/opencodecollaborative21app/activities/MainActivity.java
+++ app/src/main/java/com/example/opencodecollaborative21app/activities/MainActivity.java
@@ -1,19 +1,25 @@
 package com.example.opencodecollaborative21app.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import api.FetchApiSingleton;
 
 import com.example.opencodecollaborative21app.R;
 
-import api.FetchApiSingleton;
-
 public class MainActivity extends AppCompatActivity {
+    NavController navController;
     FetchApiSingleton fetchApiSingleton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        navController= Navigation.findNavController(this,R.id.fragmentContainerView);
+        NavigationUI.setupActionBarWithNavController(this, navController);
         fetchApiSingleton = new FetchApiSingleton(this);
 
         //This was the link used to test the code. URL will have to be passed as a parameter and
@@ -21,4 +27,5 @@ public class MainActivity extends AppCompatActivity {
 
         //fetchApiSingleton.FetchApi("https://api.publicapis.org/entries");
     }
+
 }
