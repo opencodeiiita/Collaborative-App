fc16c6b108416c762b6b2dada5423957f8d915ac
raghavgoel25 <>
2021-10-13 18:16:37 +0530

    Added Fetch API singleton class

--- app/src/main/java/com/example/opencodecollaborative21app/activities/MainActivity.java
+++ app/src/main/java/com/example/opencodecollaborative21app/activities/MainActivity.java
@@ -1,23 +1,24 @@
 package com.example.opencodecollaborative21app.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.fragment.NavHostFragment;
-import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
 import com.example.opencodecollaborative21app.R;
 
+import api.FetchApiSingleton;
+
 public class MainActivity extends AppCompatActivity {
-    NavController navController;
+    FetchApiSingleton fetchApiSingleton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        navController= Navigation.findNavController(this,R.id.fragmentContainerView);
-        NavigationUI.setupActionBarWithNavController(this, navController);
-    }
+        fetchApiSingleton = new FetchApiSingleton(this);
 
+        //This was the link used to test the code. URL will have to be passed as a parameter and
+        //data will be fetched accordingly.
+
+        //fetchApiSingleton.FetchApi("https://api.publicapis.org/entries");
+    }
 }
