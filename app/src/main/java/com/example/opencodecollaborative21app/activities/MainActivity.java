a0e98a5ad6cd072062d8c23e5e64b6107a82c853
raghavgoel25 <>
2021-10-14 10:52:11 +0530

    Made changes

--- app/src/main/java/com/example/opencodecollaborative21app/activities/MainActivity.java
+++ app/src/main/java/com/example/opencodecollaborative21app/activities/MainActivity.java
@@ -1,24 +1,57 @@
+//package com.example.opencodecollaborative21app.activities;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//import android.os.Bundle;
+//
+//import com.example.opencodecollaborative21app.R;
+//
+//import api.FetchApiSingleton;
+//
+//public class MainActivity extends AppCompatActivity {
+//    FetchApiSingleton fetchApiSingleton;
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        fetchApiSingleton = new FetchApiSingleton(this);
+//
+        //This was the link used to test the code. URL will have to be passed as a parameter and
+        //data will be fetched accordingly
+
+        //fetchApiSingleton.FetchApi("https://api.publicapis.org/entries");
+//    }
+//}
 package com.example.opencodecollaborative21app.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
+import com.example.opencodecollaborative21app.api.FetchApiSingleton;
+
 import com.example.opencodecollaborative21app.R;
 
-import api.FetchApiSingleton;
-
 public class MainActivity extends AppCompatActivity {
+    NavController navController;
     FetchApiSingleton fetchApiSingleton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        navController = Navigation.findNavController(this, R.id.fragmentContainerView);
+        NavigationUI.setupActionBarWithNavController(this, navController);
         fetchApiSingleton = new FetchApiSingleton(this);
 
         //This was the link used to test the code. URL will have to be passed as a parameter and
         //data will be fetched accordingly
 
-        //fetchApiSingleton.FetchApi("https://api.publicapis.org/entries");
+        fetchApiSingleton.FetchApi("https://opencodeiiita.herokuapp.com/get-issue-assigned/");
     }
+
 }
