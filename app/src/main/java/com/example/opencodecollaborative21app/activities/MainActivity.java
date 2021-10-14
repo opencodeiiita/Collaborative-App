f7251d79717370d2760b302ef692d8e4bd4ed302
raghavgoel25 <75788719+raghavgoel25@users.noreply.github.com>
2021-10-14 11:02:19 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/opencodecollaborative21app/activities/MainActivity.java
+++ app/src/main/java/com/example/opencodecollaborative21app/activities/MainActivity.java
@@ -1,27 +1,3 @@
-//package com.example.opencodecollaborative21app.activities;
-//
-//import androidx.appcompat.app.AppCompatActivity;
-//
-//import android.os.Bundle;
-//
-//import com.example.opencodecollaborative21app.R;
-//
-//import api.FetchApiSingleton;
-//
-//public class MainActivity extends AppCompatActivity {
-//    FetchApiSingleton fetchApiSingleton;
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//        fetchApiSingleton = new FetchApiSingleton(this);
-//
-        //This was the link used to test the code. URL will have to be passed as a parameter and
-        //data will be fetched accordingly
-
-        //fetchApiSingleton.FetchApi("https://api.publicapis.org/entries");
-//    }
-//}
 package com.example.opencodecollaborative21app.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
