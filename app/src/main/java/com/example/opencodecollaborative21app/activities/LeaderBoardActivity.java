b3463e6a239b41f32fc860a5ae600bfb13eb2f6f
diyawaghmare <diya.waghmare@gmail.com>
2021-10-14 10:48:01 +0530

    Created Leaderboard Activity

---
+++ app/src/main/java/com/example/opencodecollaborative21app/activities/LeaderBoardActivity.java
@@ -0,0 +1,16 @@
+package com.example.opencodecollaborative21app.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.opencodecollaborative21app.R;
+
+public class LeaderBoardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.leaderboard);
+    }
+}
