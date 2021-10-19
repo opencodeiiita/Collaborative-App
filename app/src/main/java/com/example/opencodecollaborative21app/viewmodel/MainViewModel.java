43293cfa9e7ce1d528ff0156c5ffa40843afffaf
raghavgoel25 <>
2021-10-20 01:03:34 +0530

    Resolve

---
+++ app/src/main/java/com/example/opencodecollaborative21app/viewmodel/MainViewModel.java
@@ -0,0 +1,11 @@
+package com.example.opencodecollaborative21app.viewmodel;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    
+    public MainViewModel(){
+
+    }
+   
+}
